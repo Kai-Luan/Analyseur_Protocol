2aececab12a325003dcd99cfca9d642a39d4d45f
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-12-08 17:14:56 +0100

    Interface graphique améliorée

--- src/pobj/GUI.java
+++ src/pobj/GUI.java
@@ -1,14 +1,17 @@
 package pobj;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -21,60 +24,61 @@ import java.awt.GridLayout;
 
 // Affiche dans une Fenêtre les trames et sa descrption
 public class GUI {
+	// Interface Graphique
 	JFrame frame = new JFrame();
-	JPanel mainPanel = new JPanel(new BorderLayout());
-	JScrollPane contientTrame;
-	JScrollPane contientAnalyse;
-	JScrollPane select;
-	JTextArea text;
-	JTextArea trame;
-	JTextArea analyse;
+	JPanel boutons = new JPanel(new GridLayout(10, 0));
+	JTextArea text, trame, analyse;
 	JLabel num_trame;
+	// Liste des trames avec leurs descriptions
 	List<Trame> trames =  new ArrayList<>();;
 	List<Donnees> donnees =  new ArrayList<>();;
-	int i=0;
 	
-	public GUI() {
-		String data;
-		try {
-			List<String> listString =Parser.parser("data/message.txt");
-			for (String s: listString) {
-				Donnees d = new Donnees(s);
-				Trame t = new Trame(d);
-				donnees.add(d);
-				trames.add(t);
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	public static void main(String[] args) {		
+		new GUI();
+	}
+	// Constructeur d'interface graphique
+	public GUI() {	
+		// Ajout de la fenêtre principale
+		JPanel mainPanel = new JPanel(new BorderLayout());
 		frame.add(mainPanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Analyseur Protocol");
+		frame.setTitle("Analyseur Protocol: message.txt");
+		// Creation de la barre de  menu
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem loadItem = new JMenuItem("Load");
+		JMenuItem saveItem = new JMenuItem("Save");
+		// ajout de Load : fonctionnalité
+		loadItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectFile();
+				refreshBoutons();
+				trame.setText("");
+				analyse.setText("");
+			}
+		});
+		// Ajout du menu dans la fenêtre
+		fileMenu.add(loadItem);
+		fileMenu.add(saveItem);
+		menuBar.add(fileMenu);
+		frame.setJMenuBar(menuBar);
 		
-		// Boutons
-		JPanel boutons = new JPanel(new GridLayout(trames.size(), 0));
-		for (i=0; i<trames.size(); i++) {
-			JButton button = new JButton(String.format("Trame n° %d", i+1));
-			button.addActionListener(new A(i));
-			boutons.add(button);
-		}
 		// Ajout des Boutons dans la zones
-		select = new JScrollPane(boutons);
-		select.setPreferredSize(new Dimension(400, 450));
-		
-		// Zone des affichages de octets
-		trame= new JTextArea();
-		trame.setEditable(false);
-		contientTrame = new JScrollPane(trame);
-		contientTrame.setPreferredSize(new Dimension(500, 500));
+		JScrollPane select = new JScrollPane(boutons);
+		select.setPreferredSize(new Dimension(200, 500));
 		
 		// Zone des analyse
 		analyse= new JTextArea();
 		analyse.setEditable(false);
-		contientAnalyse = new JScrollPane(analyse);
-		contientAnalyse.setPreferredSize(new Dimension(500, 500));
+		JScrollPane contientAnalyse = new JScrollPane(analyse);
+		contientAnalyse.setPreferredSize(new Dimension(400, 500));
+		
+		// Zone des affichages de octets
+		trame= new JTextArea();
+		trame.setEditable(false);
+		JScrollPane contientTrame = new JScrollPane(trame);
+		contientTrame.setPreferredSize(new Dimension(350, 400));
 		
 		// Ajouts des zones de texte dans la fenêtre principale
 		mainPanel.add(select, BorderLayout.WEST);
@@ -90,6 +94,51 @@ public class GUI {
 		frame.pack();
 	}
 	
+	// Selection d'un fichier
+	private void selectFile() {
+		JFileChooser filechooser= new JFileChooser();
+		filechooser.setCurrentDirectory(new File("."));
+		int response = filechooser.showOpenDialog(null);
+		if (response != JFileChooser.APPROVE_OPTION) return;
+		File file =filechooser.getSelectedFile();
+		frame.setTitle("Analyseur Protocol: "+ file.getName());
+		String path = file.getAbsolutePath();
+		refreshTrames(path);
+	}
+	// Recalcule les trames du nouveau fichier donné par path
+	private void refreshTrames(String path) {
+		trames.clear();
+		donnees.clear();
+		try {
+			List<String> listString =Parser.parser(path);
+			for (String s: listString) {
+				Donnees d = new Donnees(s);
+				Trame t = new Trame(d);
+				donnees.add(d);
+				trames.add(t);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	// Refait les boutons si on a un nouveau fichier
+	private void refreshBoutons() {
+		int taille;
+		if (trames.size()>=10) taille = trames.size();
+		else taille = 10;
+		boutons.removeAll();
+		boutons.setLayout(new GridLayout(taille, 0));
+		for (int i=0; i<trames.size(); i++) {
+			JButton button = new JButton(String.format("Trame n° %d", i+1));
+			button.addActionListener(new A(i));
+			boutons.add(button);
+		}
+		boutons.revalidate();
+		boutons.repaint();
+	}
+	// Classe qui gère l'évenement si on clique sur le bouton
+	// On affiche la trame et sa description dans les zones de textes correspondantes
 	private class A implements ActionListener{
 		int indice;
 		public A(int i) {
@@ -101,9 +150,5 @@ public class GUI {
 			trame.setText(donnees.get(indice).toString());
 			num_trame.setText(String.format("Trame  n° %d ", indice+1));
 		}
-	}
-	
-	public static void main(String[] args) {		
-		new GUI();
 	}
 }

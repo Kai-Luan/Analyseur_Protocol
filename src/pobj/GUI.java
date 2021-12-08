bc6fb981c0bc1c703bd48e6a8779e2fa9ad4ac1d
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-12-08 01:14:01 +0100

    Interface graphique

--- src/pobj/GUI.java
+++ src/pobj/GUI.java
@@ -1,66 +1,109 @@
 package pobj;
 import java.awt.Dimension;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.StringJoiner;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+
+import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+
+import java.awt.GridLayout;
 
 // Affiche dans une Fenêtre les trames et sa descrption
 public class GUI {
 	JFrame frame = new JFrame();
-	JPanel mainPanel = new JPanel();
+	JPanel mainPanel = new JPanel(new BorderLayout());
 	JScrollPane contientTrame;
-	JScrollPane contientData;
+	JScrollPane contientAnalyse;
+	JScrollPane select;
 	JTextArea text;
 	JTextArea trame;
+	JTextArea analyse;
+	JLabel num_trame;
+	List<Trame> trames =  new ArrayList<>();;
+	List<Donnees> donnees =  new ArrayList<>();;
+	int i=0;
 	
-	// L'interface graphique pour afficher la description de la trame
-	public GUI(String data, String octets) {
-		mainPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+	public GUI() {
+		String data;
+		try {
+			List<String> listString =Parser.parser("data/message.txt");
+			for (String s: listString) {
+				Donnees d = new Donnees(s);
+				Trame t = new Trame(d);
+				donnees.add(d);
+				trames.add(t);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		frame.add(mainPanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Our GUI");
+		frame.setTitle("Analyseur Protocol");
 		
-		// Affiche l'analyse de la trame
-		text= new JTextArea(data);
-		text.setEditable(false);
-		contientData = new JScrollPane(text);
-		contientData.setPreferredSize(new Dimension(400, 450));
+		// Boutons
+		JPanel boutons = new JPanel(new GridLayout(trames.size(), 0));
+		for (i=0; i<trames.size(); i++) {
+			JButton button = new JButton(String.format("Trame n° %d", i+1));
+			button.addActionListener(new A(i));
+			boutons.add(button);
+		}
+		// Ajout des Boutons dans la zones
+		select = new JScrollPane(boutons);
+		select.setPreferredSize(new Dimension(400, 450));
 		
-		// Affiche les octets de la trame
-		trame= new JTextArea(octets);
+		// Zone des affichages de octets
+		trame= new JTextArea();
 		trame.setEditable(false);
 		contientTrame = new JScrollPane(trame);
 		contientTrame.setPreferredSize(new Dimension(500, 500));
 		
+		// Zone des analyse
+		analyse= new JTextArea();
+		analyse.setEditable(false);
+		contientAnalyse = new JScrollPane(analyse);
+		contientAnalyse.setPreferredSize(new Dimension(500, 500));
+		
 		// Ajouts des zones de texte dans la fenêtre principale
-		mainPanel.add(contientData);
-		mainPanel.add(contientTrame);
+		mainPanel.add(select, BorderLayout.WEST);
+		mainPanel.add(contientAnalyse, BorderLayout.CENTER);
+		mainPanel.add(contientTrame, BorderLayout.EAST);
+		
+		// Ajout Label
+		num_trame = new JLabel("Trame  n°  ", SwingConstants.CENTER);
+		mainPanel.add(num_trame, BorderLayout.NORTH);
 		
 		frame.setSize(1000, 1000);
 		frame.setVisible(true);
 		frame.pack();
 	}
 	
+	private class A implements ActionListener{
+		int indice;
+		public A(int i) {
+			indice = i;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			analyse.setText(trames.get(indice).toString());
+			trame.setText(donnees.get(indice).toString());
+			num_trame.setText(String.format("Trame  n° %d ", indice+1));
+		}
+	}
+	
 	public static void main(String[] args) {		
-		String data;
-		try {
-			List<String> s =Parser.parser("data/trame_DNS.txt");
-			Donnees octets= new Donnees(s.get(0));
-			Trame t = new Trame(octets);
-			data = t.toString();
-			//new GUI(data, octets.toString());
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+		new GUI();
 	}
 }
